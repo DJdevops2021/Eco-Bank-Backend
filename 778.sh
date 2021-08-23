c47aca9a15b2725c9e1160e80c628f63060aabf1
DJ <divinejumi@yahoo.com>
2021-08-23 17:50:38 +0000

    My first bulk commit

---
+++ 778.sh
@@ -0,0 +1,15 @@
+echo "please enter filename"
+read filename
+sort $filename
+$filename ()
+{ 
+if [ file exists ]
+then
+echo "it exists. please proceed..."
+grep $filename
+else
+echo "it doesn't exist"
+fi
+}
+$filename
+
---
+++ test.sh
@@ -0,0 +1,6 @@
+echo "please enter filename"
+read filename
+ls -l
+cat $filename
+tree $filename
+echo "file created successfully"
